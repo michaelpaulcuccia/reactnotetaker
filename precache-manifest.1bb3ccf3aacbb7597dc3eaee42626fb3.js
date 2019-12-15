df2f37250cda7d58abc158e02b9336c3586e4c7b
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2019-12-15 08:15:49 -0500

    Updates

---
+++ precache-manifest.1bb3ccf3aacbb7597dc3eaee42626fb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef58471bcecfe9cc99f90bad26580f36",
+    "url": "/reactnotetaker/index.html"
+  },
+  {
+    "revision": "d5e02089f5b5db935fdd",
+    "url": "/reactnotetaker/static/css/main.db903ed9.chunk.css"
+  },
+  {
+    "revision": "5c3eb1e4021b90d0aa56",
+    "url": "/reactnotetaker/static/js/2.07cab67c.chunk.js"
+  },
+  {
+    "revision": "d5e02089f5b5db935fdd",
+    "url": "/reactnotetaker/static/js/main.a1de296c.chunk.js"
+  },
+  {
+    "revision": "27495777b47a10857812",
+    "url": "/reactnotetaker/static/js/runtime-main.932bc541.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactjsnotetaker/precache-manifest.f97cd74e98a3043d0f4fc5d5bd226322.js"
+  "/reactnotetaker/precache-manifest.1bb3ccf3aacbb7597dc3eaee42626fb3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjsnotetaker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactnotetaker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
